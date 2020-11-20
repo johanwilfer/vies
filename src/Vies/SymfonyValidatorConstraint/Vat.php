8ebe0a9e3e000f33f135852406c8b18eafc873f4
Johan Wilfer <johan@jttech.se>
2020-11-20 17:07:46 +0100

    Work in progress

--- src/Vies/SymfonyValidatorConstraint/Vat.php
+++ src/Vies/SymfonyValidatorConstraint/Vat.php
@@ -8,8 +8,29 @@ use Symfony\Component\Validator\Constraint;
 
 /**
  * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ *
+ * @author Johan Wilfer <johan@jttech.se>
  */
 class Vat extends Constraint
 {
     public $message = 'Not valid';
+
+    //4. Not registered for VAT Ok
+    //5. Valid / not valid (checksum)
+
+
+    //1. Not inside country
+    //2. Required for EU countries
+    //3. Should not be given for non-EU contries
+    public $countryField = null;
+
+    public $mode; // "online", "checksum", "online_checksum"
+    //
+    //online only
+    //checksum only
+    //online + checksum
+    //
+
+    // not registered for VAT?
 }
--- src/Vies/SymfonyValidatorConstraint/VatValidator.php
+++ src/Vies/SymfonyValidatorConstraint/VatValidator.php
@@ -4,6 +4,8 @@ declare (strict_types=1);
 
 namespace DragonBe\Vies\SymfonyValidatorConstraint;
 
+use DragonBe\Vies\Vies;
+use DragonBe\Vies\ViesServiceException;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
@@ -42,6 +44,51 @@ class VatValidator extends ConstraintValidator
             // the argument must be a string or an object implementing __toString()
             $this->context->buildViolation($constraint->message)
                 ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+
+
+        // first we look at the country the user has selected
+        // we need country to validate VAT
+        $vies = new Vies();
+
+        $vatCountry = substr($value, 0, 2);
+        $vatId = substr($value, 2);
+
+        // Greece uses EL as a VAT prefix, but the country code is GR. Make adjustments for this.
+        // https://github.com/DragonBe/vies/issues/57
+        $euCountries = $vies->listEuropeanCountries();
+        $euCountries['GR'] = $euCountries['EL'];
+        unset($euCountries['EL']);
+
+        // we check only countries from UE
+        if (!array_key_exists($country, $euCountries)) {
+            return;
+        }
+
+        if ($vatCountry !== $country) {
+            // if vat number is not starting with country code from billing country starting from invalid country
+            $this->context->buildViolation('sirvoy_affiliate.vat_inside_contry')
+                ->setTranslationDomain('validators')
+                ->atPath($propertyName)
+                ->addViolation();
+
+            // if we get this error we are not needed searching for other errors (not need to ask online service)
+            return;
+        }
+
+        // now we validate the vatNo - try online validation first, fallback to checksum if we get Exception
+        try {
+            $result = $vies->validateVat($vatCountry, $vatId);
+            $vatIdValid = $result->isValid();
+        } catch (ViesServiceException $e) {
+            $vatIdValid = $vies->validateVatSum($vatCountry, $vatId);
+        }
+
+        if (!$vatIdValid) {
+            $this->context->buildViolation('sirvoy_affiliate.vat_invalid')
+                ->setTranslationDomain('validators')
+                ->atPath($propertyName)
                 ->addViolation();
         }
     }
